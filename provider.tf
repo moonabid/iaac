bbf74bc9a4dc36e95a9b5d1f8f937e5b196226aa
ABID Moon <60834531+moonabid@users.noreply.github.com>
2023-10-20 03:46:31 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
    backend "s3" {
-     bucket = "futuiontech-terraform"
+     bucket = "futuiontech-terraformz"
      key    = ".terraform.tfstate"
      region = "ap-south-1"
    }
