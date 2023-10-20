0bbe0dfe4b72c2f3c8a75b70e0dcb2907c474a38
ABID Moon <60834531+moonabid@users.noreply.github.com>
2023-10-20 03:27:56 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "aws_mfh_ec2" {
 
-  ami                         = "ami-0da59f1af71ea4ad2"
+  ami                         = "ami-09a81b370b76de6a2"
   instance_type               = "t2.micro"
   vpc_security_group_ids      = [aws_security_group.aws_sg.id]
   associate_public_ip_address = true
